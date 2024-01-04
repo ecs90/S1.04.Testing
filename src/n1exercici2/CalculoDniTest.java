b4115279854d64e82cf8c01731809db43b7a82f4
ecs90 <ecs_salas@hotmail.com>
2024-01-04 18:02:20 +0100

    n1e2 test parametrizado

--- src/n1exercici2/CalculoDniTest.java
+++ src/n1exercici2/CalculoDniTest.java
@@ -2,11 +2,13 @@ package n1exercici2;
 
 import n1exercici1.Any;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculoDniTest {
-    @Test
+    /*@Test
     public void testPositions() throws Throwable{
         CalculoDni dni = new CalculoDni(60178303);
 
@@ -39,5 +41,15 @@ public class CalculoDniTest {
         dni.setNumber(15926483);
         dni.calcularLletra();
         assertEquals('H', dni.getLletra(), (String) null);
+    }*/
+
+    @ParameterizedTest
+    @ValueSource(strings = {"60178303E", "60145937V", "36442120T", "18648966Z", "16759345G", "13798264N", "12587469Y",
+            "14789632B", "12369874Z", "15926483H"})
+    public void testCalcularLetra(String documento) {
+        CalculoDni calculodni = new CalculoDni(Integer.parseInt(documento.substring(0, 8)));
+        Character letraEsperada = documento.charAt(8);
+        calculodni.calcularLletra();
+        assertEquals(letraEsperada, calculodni.getLletra());
     }
 }
